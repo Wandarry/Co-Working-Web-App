1fd98224c8aa32557d17e8fa9fbae5e22205f29a
Wandarry <berryenick20@gmail.com>
2023-04-11 14:38:41 +0100

    install package for icons and create header

---
+++ src/screen/landingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './landingPage.css';
+import { Header } from "../components";
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default LandingPage;
